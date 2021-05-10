8838de8bc604c64a02c56ad19d763ee37a408e0f
Marco <marcoerhardt95@gmail.com>
2021-05-10 15:32:33 +0200

    Add new scene SceneNameInput, make it accept users name and store it in localstorage

--- src/index.js
+++ src/index.js
@@ -86,17 +86,14 @@ class SceneMainMenu extends Phaser.Scene {
   }
   create() {
     hideStatuses();
-    this.play = this.add.text(
-      554,
-      237.5,
-      `PLAY`,
+    this.play = this.add.text(554,237.5,`PLAY`,
       {
         fontSize: '40px',
         fill: '#FFFFFF',
-      },
-    ).setInteractive();
+      })
+      .setInteractive();
     this.play.on('pointerdown', () => {
-      this.scene.start('SceneMain');
+      this.scene.start('SceneNameInput');
     });
 
     this.welcomeText = this.add.text(
@@ -109,6 +106,25 @@ class SceneMainMenu extends Phaser.Scene {
       },
     );
     this.welcomeText.setOrigin(0.5, 0.5);
+  }
+}
+
+class SceneNameInput extends Phaser.Scene {
+  constructor() {
+    super({ key: "SceneNameInput" });
+  }
+  create() {
+    showForm();
+    const button = document.getElementById('button');
+    button.addEventListener('click', () => {
+      const nameInput = document.getElementById('nameInput').value;
+      if (nameInput == null || nameInput === '') {
+        localStorage.setItem('username:', JSON.stringify('Anon'));} 
+      else {
+        localStorage.setItem('username:', JSON.stringify(nameInput));}
+      this.scene.start('SceneMain');
+      hideForm();
+    });
   }
 }
 
@@ -684,6 +700,7 @@ var config = {
   },
   scene: [
     SceneMainMenu,
+    SceneNameInput,
     SceneMain,
     SceneGameOver
   ],
@@ -840,6 +857,17 @@ const showStatuses = () => {
   scoreField.classList.add('display-flex');
   healthBar.classList.add('display-flex');
   time.classList.add('display-block');
+}
+
+const form = document.getElementById('form');
+const showForm = () => {
+  form.classList.remove('display-none');
+  form.classList.add('display-flex');
+}
+
+const hideForm = () => {
+  form.classList.remove('display-flex');
+  form.classList.add('display-none');
 }
 
 function hitBomb (player, bomb)
